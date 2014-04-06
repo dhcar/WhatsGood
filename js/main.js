583b782eb7ce119be15ddd55d491a58933ef7cdc
David Helgeson <david.helgeson89@gmail.com>
2014-04-06 14:22:35 -0700

    y

--- js/main.js
+++ js/main.js
@@ -73,24 +73,24 @@ var app = {
 
 	self: this,
 
-	// auth: {},
+	auth: {},
 
 	ref: new Firebase('https://whatsgood.firebaseio.com/'),
 
-	// user: {},
+	user: {},
 
-	// posts: {},
+	posts: {},
 
-	// events: {},
+	events: {},
 
-	// friends: {},
+	friends: {},
 
-	// invites: {},
+	invites: {},
 
 	init: function(){
 		// read
 		// this.ref.child('users').child(app.user.id).on('value', this.setUserInfo);
-		app.ref.child('recentPosts').child(app.user.id).on('child_added', app.getPosts);
+		app.ref.child(app.user.id).on('child_added', app.getPosts);
 		app.ref.child('private').child(app.user.id).child('friends').on('child_added', app.makeFriends);
 		app.ref.child('private').child(app.user.id).child('events').on('child_added', app.combEvents);
 		app.ref.child('invites').child(app.user.id).on('child_added', app.makeInvites);
