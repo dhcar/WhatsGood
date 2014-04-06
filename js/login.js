895004922952dc30d957298bed6d515c545bc6e8
David Helgeson <david.helgeson89@gmail.com>
2014-04-06 09:51:41 -0700

    y

--- js/login.js
+++ js/login.js
@@ -13,32 +13,34 @@ var	auth = new FirebaseSimpleLogin(ref, function(error, user){
 // <input type="password" id="password">
 // <button id='login'>Log In</button><button id="newUser">New User</button>
 
-document.getElementById('login').addEventListener('click', function(){
-	var password = document.getElementById('password').value;
-	var email    = document.getElementById('email').value;
-	auth.login('password', {
-  		email: email,
-  		password: password,
-  		rememberMe: true
+$(document).ready(function(){
+	document.getElementById('login').addEventListener('click', function(){
+		var password = document.getElementById('password').value;
+		var email    = document.getElementById('email').value;
+		auth.login('password', {
+	  		email: email,
+	  		password: password,
+	  		rememberMe: true
+		});
+		window.location.replace('index.html');
 	});
-	window.location.replace('index.html');
-});
-
-document.getElementById('newUser').addEventListener('click', function(){
-	var password = document.getElementById('passwordR').value;
-	var email    = document.getElementById('emailR').value;
-	auth.createUser(email, password, function(error, user){
-		if (error) {
-			console.log(error);
-		} else {
-			ref.child('users').child(user.id).set({email: email, name: name});
-			console.log(user);
-			auth.login('password', {
-  				email: email,
-  				password: password,
-  				rememberMe: true
-			});
-			window.location.replace('index.html');
-		}
+	
+	document.getElementById('newUser').addEventListener('click', function(){
+		var password = document.getElementById('passwordR').value;
+		var email    = document.getElementById('emailR').value;
+		auth.createUser(email, password, function(error, user){
+			if (error) {
+				console.log(error);
+			} else {
+				ref.child('users').child(user.id).set({email: email, name: name});
+				console.log(user);
+				auth.login('password', {
+	  				email: email,
+	  				password: password,
+	  				rememberMe: true
+				});
+				window.location.replace('index.html');
+			}
+		});
 	});
 });
