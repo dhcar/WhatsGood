e8d602ffd4f7fdb362d441ca89753a0e43438b38
Kayvan Boudai <kboudai@ucsc.edu>
2014-04-05 17:27:27 -0700

    added finding location pushing for test

--- js/Maps.js
+++ js/Maps.js
@@ -1,8 +1,47 @@
+var map;
+
 function initialize() {
-	var mapOptions = {
-		center: new google.maps.LatLng(36.989031,-122.06446),
-		zoom: 15
-	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-		mapOptions);
+  var mapOptions = {
+    zoom: 15
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
