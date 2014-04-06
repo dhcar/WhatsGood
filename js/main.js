16fc1168afde48bd7c88c9a45152dab94bb81111
David Helgeson <david.helgeson89@gmail.com>
2014-04-06 14:44:05 -0700

    y

--- js/main.js
+++ js/main.js
@@ -311,6 +311,8 @@ var app = {
     	    if (error) {
     	        console.log('doLogin');
     	        console.log(error);
+    	        auth.logout();
+    	        window.location.reload('login.html');
     	    } else if (!error) {
     	        console.log("user.id: " + user.id);
     	        if (user.id) {
