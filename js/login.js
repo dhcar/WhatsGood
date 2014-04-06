24e0bdb5bf4d5ec6fef374efbfe340a33ba1b1fc
David Helgeson <david.helgeson89@gmail.com>
2014-04-06 11:03:39 -0700

    y

--- js/login.js
+++ js/login.js
@@ -17,19 +17,44 @@ function stringifyUser(obj){
 	localStorage.setItem('_user',_user);
 }
 
+function doLogin(){
+	var password = document.getElementById('password').value || document.getElementById('passwordR').value;
+	var email    = document.getElementById('email').value || document.getElementById('emailR').value;
+    var authLogin = new FirebaseSimpleLogin(ref, function(error, user) {
+        if (error) {
+            console.log('doLogin');
+            console.log(error);
+        } else if (!error) {
+            authLogin.login('password', {
+                email: email,
+                password: password,
+                rememberMe: true
+            });
+            console.log("user.id: " + user.id);
+            if (user.id) {
+                console.log("fbUserId: " + fbUserId);
+                window.location.redirect("index.html");
+            }
+        }
+    });
+}
+
 $(document).ready(function(){
-	document.getElementById('login').addEventListener('click', function(){
-		var password = document.getElementById('password').value;
-		var email    = document.getElementById('email').value;
-		auth.login('password', {
-	  		email: email,
-	  		password: password,
-	  		rememberMe: true
-		});
-		if(auth.id){
-			window.location.replace('index.html');
-		}
-	});
+	// var password = document.getElementById('password').value;
+	// var email    = document.getElementById('email').value;
+
+	document.getElementById('login').addEventListener('click', doLogin);
+
+	// document.getElementById('login').addEventListener('click', function(){
+	// 	auth.login('password', {
+	//   		email: email,
+	//   		password: password,
+	//   		rememberMe: true
+	// 	});
+	// 	if(auth.id){
+	// 		window.location.replace('index.html');
+	// 	}
+	// });
 	
 	document.getElementById('newUser').addEventListener('click', function(){
 		var name     = document.getElementById('nameR').value;
@@ -40,12 +65,13 @@ $(document).ready(function(){
 				console.log(error);
 			} else {
 				ref.child('users').child(user.id).set({email: email, name: name});
-				console.log(user);
-				auth.login('password', {
-	  				email: email,
-	  				password: password,
-	  				rememberMe: true
-				});
+				// console.log(user);
+				// auth.login('password', {
+	  	// 			email: email,
+	  	// 			password: password,
+	  	// 			rememberMe: true
+				// });
+				doLogin();
 				if(auth.id){
 					window.location.replace('index.html');
 				}
