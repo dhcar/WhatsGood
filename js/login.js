09217f32b1a295f28e24004fbdb05e4d3b17745f
David Helgeson <david.helgeson89@gmail.com>
2014-04-06 09:25:48 -0700

    y

--- js/login.js
+++ js/login.js
@@ -3,13 +3,8 @@ var ref = new Firebase('https://whatsgood.firebaseio.com/');
 var	auth = new FirebaseSimpleLogin(ref, function(error, user){
 	if (error) {
 		console.log(error);
-		auth.login('facebook', {
-  			rememberMe: true,
-  			scope: 'email'
-		});
 	} else if (user) {
 		console.log(user);
 		console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
-		window.location.replace('index.html');
 	}
 });
