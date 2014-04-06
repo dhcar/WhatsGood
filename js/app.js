e5793f6930aae8c4976008482fa30ee21d79aa7a
David Helgeson <david.helgeson89@gmail.com>
2014-04-05 18:58:37 -0700

    Merge branch 'gh-pages' of https://github.com/dhcar/WhatsGood into gh-pages
    Conflicts:
    js/Maps.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,46 @@
 // App logic
 
+var fbUrl = 'https://whatsgood.firebaseio.com/';
+var fbRef = new Firebase(fburl);
+
+var user  = {};
+
+if (!auth) {
+	var auth = new FirebaseSimpleLogin(fbUrl, function(error, userSnap) {
+		if (error){
+			console.log(error);
+		} else {
+			user = userSnap;
+		}
+	});
+}
+
+
+// utils
+function elt(tag, content, attrs) {
+   var e = document.createElement(tag);
+   if (typeof content === "string") {
+     setTextContent(e, content);
+   } else if (content) {
+     for (var i = 0; i < content.length; ++i) { e.appendChild(content[i]); }
+   }
+   for(var attr in (attrs || { })) {
+     e.setAttribute(attr, attrs[attr]);
+   }
+   return e;
+ }
+
+ function setTextContent(e, str) {
+   e.innerHTML = "";
+   e.appendChild(document.createTextNode(str));
+ }
+
+ function randomString(length, chars) {
+    var mask = '';
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (chars.indexOf('#') > -1) mask += '0123456789';
+    var result = '';
+    for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
+    return result;
+}
--- js/invites.js
+++ js/invites.js
@@ -1,18 +1,11 @@
-// 
-// 
-;$(document).load(makeInvite);
-var makeInvite = (function(document,window, undefined) {
+;var makeInvite = (function(document,window, undefined) {
 	function makeInvite(){
 		if (!(this instanceof makeInvite)) { return new makeInvite(); }
-		var self = this;
-		this._inviteButtonLoc = document.getElementById('dag_invite_button');
-		this._selectLoc       = document.getElementById('dag_invite_selector');
-		this._userDagDrop     = document.getElementsByClassName('user-dag-invite');
-		// this._doMoreShit();
-		// this._userDagDrop.forEach(function(el, index, array){
-		// 	el.addEventListener("click", self._pushId);
-		// });
-		$('.user-dag-invite').on('click', _pushId);
+		var self              = this;
+		this._inviteButtonLoc = document.getElementById('event_invite_button');
+		this._selectLoc       = document.getElementById('event_invite_selector');
+		this._usereventDrop   = document.getElementsByClassName('user-event-invite');
+		$('.user-event-invite').on('click', _pushId);
 		this._inviteButtonLoc.addEventListener('click', _onSubmit);
 	}
 
@@ -24,23 +17,23 @@ var makeInvite = (function(document,window, undefined) {
 		// 
 		var userId       = this.id;
 		console.log(userId);
-		document.getElementById('dag_invite_selector').setAttribute("data-invitee", userId);
+		document.getElementById('event_invite_selector').setAttribute("data-invitee", userId);
 		console.log('set attribute');
 		_doMoreShit();
 	}
 
 	function _doMoreShit(){
-		var self    = this;
-		var selectLoc = document.getElementById('dag_invite_selector');
+		var self            = this;
+		var selectLoc       = document.getElementById('event_invite_selector');
 		selectLoc.innerHTML = '';
-		document.getElementById('dag_invite_button').textContent = 'Send Invite';
+		document.getElementById('event_invite_button').textContent = 'Send Invite';
 		// fill drop
 		// this._dropList = elt('', content, attrs);
-		user.ref.root().child('private').child(user.ident).child('dagz').on('value', function(snap){
+		user.ref.root().child('private').child(user.ident).child('events').on('value', function(snap){
 			snap.forEach(function(snap2){
-				var dagId   = snap2.name();
-				var dagName = snap2.child('title').val();
-				var el      = elt('option', dagName, {'value' : dagId});
+				var eventId   = snap2.name();
+				var eventName = snap2.child('title').val();
+				var el        = elt('option', eventName, {'value' : eventId});
 				selectLoc.appendChild(el);
 			});
 		});
@@ -48,28 +41,20 @@ var makeInvite = (function(document,window, undefined) {
 
 	function _onSubmit(){
 		// this element is hidden
-		var selectLoc         = document.getElementById('dag_invite_selector');
-		var userId            = selectLoc.getAttribute('data-invitee');
+		var selectLoc          = document.getElementById('event_invite_selector');
+		var userId             = selectLoc.getAttribute('data-invitee');
 		console.log(userId);
-		var select_name       = selectLoc.options[selectLoc.selectedIndex].text;
-		var select_dagId      = selectLoc.options[selectLoc.selectedIndex].value;
-		var inviteObj         = {};
-		inviteObj['sender']   = user.fullName;
-		inviteObj['senderId'] = user.ident;
-		inviteObj['dagId']    = select_dagId;
-		inviteObj['dagName']  = select_name;
-		this.textContent      = 'invited';
+		var select_name        = selectLoc.options[selectLoc.selectedIndex].text;
+		var select_eventId     = selectLoc.options[selectLoc.selectedIndex].value;
+		var inviteObj          = {};
+		inviteObj['sender']    = user.fullName;
+		inviteObj['senderId']  = user.ident;
+		inviteObj['eventId']   = select_eventId;
+		inviteObj['eventName'] = select_name;
+		this.textContent       = 'invited';
 		return user.ref.root().child('invites').child(userId).push(inviteObj);
 	}
 
 	return makeInvite;
 
 }(document, window, undefined));
-
-(function () {
-	$(document).ready(function(){
-		console.log('make invite');
-		makeInvite();
-	});
-	$('.right-off-canvas-toggle').on('click', makeInvite);
-})();
